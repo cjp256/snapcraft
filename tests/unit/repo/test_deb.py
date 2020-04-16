3bd02c60c721d11efeb4e4fe5aa8440f78b48a1c
Chris Patterson <chris.patterson@canonical.com>
2020-04-16 16:16:14 -0400

    WIP: REPOS

--- tests/unit/repo/test_deb.py
+++ tests/unit/repo/test_deb.py
@@ -210,8 +210,8 @@ class UbuntuTestCase(RepoBaseTestCase):
     def test_sources_formatting(self, mock_version_codename):
         sources_list = textwrap.dedent(
             """
-            deb http://archive.ubuntu.com/ubuntu ${release} main restricted
-            deb http://archive.ubuntu.com/ubuntu ${release}-updates main restricted
+            deb http://archive.ubuntu.com/ubuntu $release main restricted
+            deb http://archive.ubuntu.com/ubuntu $release-updates main restricted
             """
         )
 
@@ -451,7 +451,7 @@ class TestUbuntuInstallRepo(unit.TestCase):
             new=str(snapcraft_list),
         ):
             test_source = "deb http://source"
-            repo.Ubuntu.install_source(test_source)
+            repo.Ubuntu.install_source(name="test-name", source=test_source)
 
             self.assertThat(snapcraft_list.exists(), Equals(True))
             self.assertThat(snapcraft_list.owner(), Equals("root"))
@@ -463,7 +463,7 @@ class TestUbuntuInstallRepo(unit.TestCase):
             self.assertThat(test_source in installed_sources, Equals(True))
 
             test_source2 = "deb http://source2"
-            repo.Ubuntu.install_source(test_source2)
+            repo.Ubuntu.install_source(name="test-name2", source=test_source2)
 
             installed_sources = snapcraft_list.read_text().splitlines()
             expected_sources = sorted([test_source, test_source2])
@@ -472,28 +472,25 @@ class TestUbuntuInstallRepo(unit.TestCase):
 
     @mock.patch("subprocess.run")
     def test_install_gpg(self, mock_run):
-        repo.Ubuntu.install_gpg_key("FAKEKEY")
+        repo.Ubuntu.install_gpg_key(key_id="FAKEID", key="FAKEKEY")
 
-        self.assertThat(
-            mock_run.mock_calls,
-            Equals(
-                [
-                    call(
-                        [
-                            "sudo",
-                            "apt-key",
-                            "--keyring",
-                            repo.Ubuntu._SNAPCRAFT_INSTALLED_GPG_KEYRING,
-                            "add",
-                            "-",
-                        ],
-                        check=True,
-                        input=b"FAKEKEY",
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.STDOUT,
-                    )
-                ]
-            ),
+        mock_run.assert_has_calls(
+            [
+                call(
+                    [
+                        "sudo",
+                        "apt-key",
+                        "--keyring",
+                        repo.Ubuntu._SNAPCRAFT_INSTALLED_GPG_KEYRING,
+                        "add",
+                        "-",
+                    ],
+                    check=True,
+                    input=b"FAKEKEY",
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
+                )
+            ]
         )
 
     @mock.patch("subprocess.run")
@@ -503,5 +500,8 @@ class TestUbuntuInstallRepo(unit.TestCase):
         )
 
         self.assertRaises(
-            errors.AptGPGKeyInstallError, repo.Ubuntu.install_gpg_key, "FAKEKEY"
+            errors.AptGPGKeyInstallError,
+            repo.Ubuntu.install_gpg_key,
+            key_id="FAKEID",
+            key="FAKEKEY",
         )
